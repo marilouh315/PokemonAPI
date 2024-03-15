bef7721e096d2803f2536b383a4cd7a81587b63a
marilouh315 <111771090+marilouh315@users.noreply.github.com>
2024-03-15 11:52:12 -0400

    test1shawn

--- src/config/db.js
+++ src/config/db.js
@@ -1,20 +1,20 @@
-const mysql = require("mysql");
-const dotenv = require("dotenv");
-dotenv.config();
+// const mysql = require("mysql");
+// const dotenv = require("dotenv");
+// dotenv.config();
 
-const pool = mysql.createPool({
-    connectionLimit: process.env.MYSQL_CONNECTION_LIMIT,
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE
-});
+// const pool = mysql.createPool({
+//     connectionLimit: process.env.MYSQL_CONNECTION_LIMIT,
+//     host: process.env.MYSQL_HOST,
+//     user: process.env.MYSQL_USER,
+//     password: process.env.MYSQL_PASSWORD,
+//     database: process.env.MYSQL_DATABASE
+// });
 
-// open the MySQL connection
-pool.getConnection((error, connection) => {
-    if (error) throw error;
-    console.log("Successfully connected to the database.");
-    connection.release(); // Release the connection to the pool
-});
+// // open the MySQL connection
+// pool.getConnection((error, connection) => {
+//     if (error) throw error;
+//     console.log("Successfully connected to the database.");
+//     connection.release(); // Release the connection to the pool
+// });
 
-module.exports = pool;
+// module.exports = pool;
